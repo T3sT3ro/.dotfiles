4daa054f043774a2b44b6045f21141d581a1dad3
Tooster <5300963+T3sT3ro@users.noreply.github.com>
2024-06-03 16:19:21 +0200

    Added nautilus extension for opening symlink location

---
+++ .local/share/nautilus-python/extensions/open_symlink_extension.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+import os
+import subprocess
+from gi.repository import Nautilus, GObject, Gtk
+
+class GoToLinkLocationExtension(GObject.GObject, Nautilus.MenuProvider):
+    def __init__(self):
+        GObject.Object.__init__(self)
+
+    def get_file_items(self, window, files):
+        # Check if exactly one file is selected
+        if len(files) != 1:
+            return
+
+        file = files[0]
+
+        # Check if the selected file is a symbolic link
+        if not file.get_uri_scheme() == 'file' or not os.path.islink(file.get_location().get_path()):
+            return
+
+        # Create a menu item
+        item = Nautilus.MenuItem(
+            name="GoToLinkLocationExtension::GoToLinkLocation",
+            label="Go to Link Location",
+            tip="Opens the directory containing the link target"
+        )
+        item.connect('activate', self.go_to_link_location, file)
+
+        return [item]
+
+    def go_to_link_location(self, menu, file):
+        link_path = file.get_location().get_path()
+        target_path = os.readlink(link_path)
+        target_dir = os.path.dirname(target_path)
+
+        # Ensure the target directory is an absolute path
+        if not os.path.isabs(target_dir):
+            target_dir = os.path.abspath(os.path.join(os.path.dirname(link_path), target_dir))
+
+        # Open the directory containing the target
+        subprocess.Popen(['xdg-open', target_dir])
