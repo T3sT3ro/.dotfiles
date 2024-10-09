209076d4c7196520feee1219c8ccccf7d999559a
Tooster <5300963+T3sT3ro@users.noreply.github.com>
2024-10-09 19:43:32 +0200

    update wezterm leader to CTRL+\ and unbind font size binds

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -11,7 +11,7 @@ config.window_padding = {
 }
 
 config.initial_cols = 140
-config.initial_rows = 36
+config.initial_rows = 36 
 
 config.window_frame = {
   border_left_width = '1px',
@@ -23,11 +23,12 @@ config.window_frame = {
   border_bottom_color = '#2e2e2e',
   border_top_color = '#2e2e2e',
 }
+config.use_resize_increments = true
 
 config.font_size = 10
 config.font = wezterm.font 'JetBrains Mono'
 config.window_background_opacity = 0.90
-config.window_decorations = "INTEGRATED_BUTTONS|RESIZE" -- use INTEGRATED_BUTTONS when linux fixes their shit
+config.window_decorations = "RESIZE" -- use INTEGRATED_BUTTONS when linux fixes their shit
 config.enable_scroll_bar = true
 config.scrollback_lines = 10000
 config.inactive_pane_hsb = {
@@ -35,17 +36,23 @@ config.inactive_pane_hsb = {
   brightness = 0.9
 }
 
-config.leader = { key = '/', mods = 'CTRL', timeout_milliseconds = 1000 }
+-- KEYMAP CONFIG
+
+config.leader = { key = '\\', mods = 'CTRL', timeout_milliseconds = 1000 }
 
 config.keys = {
   -- send C-a on double C-a
-  { key = '/', mods = 'LEADER|CTRL',
-    action = act.SendKey { key = '/', mods = 'CTRL' }
+  { key = '\\', mods = 'LEADER|CTRL',
+    action = act.SendKey { key = '\\', mods = 'CTRL' }
   },
 
   { key = 'h', mods = 'LEADER', action = act.SplitHorizontal },
   { key = 'v', mods = 'LEADER', action = act.SplitVertical },
-  { key = 'q', mods = 'LEADER', action = act.CloseCurrentPane { confirm = true }}
+  { key = 'q', mods = 'LEADER', action = act.CloseCurrentPane { confirm = true }},
+
+  -- increase/decrease font size only with leader and  CTRL + + and CTRL + -
+  { key = '+', mods = 'SHIFT|CTRL', action = act.DisableDefaultAssignment },
+  { key = '_', mods = 'SHIFT|CTRL', action = act.DisableDefaultAssignment },
 }
 
 return config
