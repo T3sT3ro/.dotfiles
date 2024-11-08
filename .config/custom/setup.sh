12cd876d161a61963a70238219aff4cddef7534b
Tooster <5300963+T3sT3ro@users.noreply.github.com>
2024-11-08 21:39:52 +0100

    dotfiles: fix setup colors and fix cargo updater package

--- .config/custom/setup.sh
+++ .config/custom/setup.sh
@@ -8,28 +8,32 @@
 
 set -e
 
-echo "Entering home directory for safety"
+function step() {
+  echo -e "\033[1;34m$1\033[0m"
+}
+
+step "Entering home directory for safety"
 cd ~ || exit
 
-echo "Making sure core utilities are installed"
+step "Making sure core utilities are installed"
 sudo apt install -y curl wget git
 
-echo "cloning dotfiles with shallow submodules"
+step "cloning dotfiles with shallow submodules"
 git clone --recurse-submodules --shallow-submodules https://github.com/T3sT3ro/.dotfiles.git ~/.dotfiles
 
-echo "aliasing dotfiles temporarily"
+step "aliasing dotfiles temporarily"
 alias dotfiles="/usr/bin/git --git-dir=~/.dotfiles/ --work-tree=~"
 
 export DOTFILES_BACKUP_DIR=$HOME/.dotfiles-backup
 
-echo "Backing up all stock dotfiles to $DOTFILES_BACKUP_DIR"
+step "Backing up all stock dotfiles to $DOTFILES_BACKUP_DIR"
 for f in $(dotfiles checkout 2>&1 | grep -E '^\s+\.' | awk '{print $1}'); do
   cp --parents -r "$f" "$DOTFILES_BACKUP_DIR" && rm -i "$f"
 done
 
-echo "setting up local dotfiles config"
+step "setting up local dotfiles config"
 dotfiles config --local status.showUntrackedFiles no
-echo "trying to checkout dotfiles"
+step "trying to checkout dotfiles"
 
 dotfiles checkout
 
@@ -38,58 +42,58 @@ dotfiles checkout
 # PPA setup
 ################################################
 
-echo "setting up wezterm PPA"
+step "setting up wezterm PPA"
 curl -fsSL https://apt.fury.io/wez/gpg.key | sudo gpg --yes --dearmor -o /etc/apt/keyrings/wezterm-fury.gpg
 $ echo 'deb [signed-by=/etc/apt/keyrings/wezterm-fury.gpg] https://apt.fury.io/wez/ * *' | sudo tee /etc/apt/sources.list.d/wezterm.list
 
-echo "setting up gh (github CLI) PPA"
+step "setting up gh (github CLI) PPA"
 (type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y)) &&
   sudo mkdir -p -m 755 /etc/apt/keyrings &&
   wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg >/dev/null &&
   sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg &&
   echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list >/dev/null
 
-echo "updating PPA repositories"
+step "updating PPA repositories"
 sudo apt update
 
-echo "installing nala"
+step "installing nala"
 sudo apt install nala -y
 
 ################################################
 # regular package and program installation
 ################################################
 
-echo "installing packages"
+step "installing packages"
 sudo nala install -y zsh wezterm gh file-roller gnome-tweaks
 
-echo "installing fonts and nerdfonts"
+step "installing fonts and nerdfonts"
 fc-cache -fv
 
-echo "installing starship"
+step "installing starship"
 curl -sS https://starship.rs/install.sh | sh
 
-echo "installing VSCode"
+step "installing VSCode"
 dpkg -i https://code.visualstudio.com/docs/?dv=linux64_deb
 
-echo "installing neovim"
+step "installing neovim"
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
 sudo rm -rf /opt/nvim
 sudo tar -C /opt -xzf nvim-linux64.tar.gz
 
 # install rust
-echo "installing rust"
+step "installing rust"
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 # install global cargo updater
-echo "installing cargo updater"
-cargo install install-update
+step "installing cargo updater"
+cargo install cargo-update
 # update all global crates
-echo "updating and installing all global cargo packages"
+step "updating and installing all global cargo packages"
 cargo install-update --all
 
 
 
 # final upgrade
-echo "running global nala upgrade"
+step "running global nala upgrade"
 sudo nala upgrade
 
 
@@ -97,13 +101,13 @@ sudo nala upgrade
 # Dconf - system settings management
 ################################################
 
-echo "dumping dconf settings"
+step "dumping dconf settings"
 dconf dump / > "$DOTFILES_BACKUP_DIR"/dconf.conf
 
-echo "loading global dconf settings"
+step "loading global dconf settings"
 dconf load / < ~/.dotfiles/dconf-dump.conf
 
-echo "entering zsh"
+step "entering zsh"
 exec zsh
 
 
