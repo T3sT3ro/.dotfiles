de104e58d64f10c571ef918c0beaa564c2e24aaf
Tooster <5300963+T3sT3ro@users.noreply.github.com>
2024-11-12 22:20:41 +0100

    nvim: try to configure cmp for command entry

--- .config/nvim/lua/configs/cmp.lua
+++ .config/nvim/lua/configs/cmp.lua
@@ -1,31 +1,39 @@
 local cmp = require "cmp"
 
+-- IMPORTANT:
+-- THIS FILE DOESN'T HAVE ANY EFFECT NOW CUZ IT'S NOT SOURCED IN init.lua!!!
+
 
 -- this doesn't work for some reason
 --- @type cmp.ConfigSchema
 local options = {
-    mapping = {
-      ['<Down>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-      ['<Up>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-    },
+  mapping = {
+    ['<Down>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+    ['<Up>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+  },
 
-    sources = {
-      "codeium"
-    }
+  sources = {
+    "codeium"
+  }
 }
 
 cmp.setup.cmdline(':', {
-      mapping = cmp.mapping.preset.cmdline(),
-      sources = cmp.config.sources({
-        { name = 'path' }
-      }, {
-        {
-          name = 'cmdline',
-          option = {
-            ignore_cmds = { 'Man', '!' }
-          }
-        }
-      })
-    })
+  mapping = cmp.mapping.preset.cmdline({
+    ['<Down>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+    ['<Up>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+    ['<Tab>'] = cmp.mapping.confirm({ select = true }), -- Confirm selection with Tab
+    ['<CR>'] = cmp.mapping.confirm({ select = false })  -- Insert without selection on Enter
+  }),
+  sources = cmp.config.sources({
+    { name = 'path' }
+  }, {
+    {
+      name = 'cmdline',
+      option = {
+        ignore_cmds = { 'Man', '!' }
+      }
+    }
+  })
+})
 
 return options
